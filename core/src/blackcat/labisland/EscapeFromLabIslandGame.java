6f0ac600a0984a0f4fe174800fd58ac65589612c
Jeff Dezso <jdezso@gmail.com>
2014-07-20 11:59:52 -0700

    Artemis Test
    First classes to baby step into using Artemis

--- core/src/blackcat/labisland/EscapeFromLabIslandGame.java
+++ core/src/blackcat/labisland/EscapeFromLabIslandGame.java
@@ -1,12 +1,12 @@
 package blackcat.labisland;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class EscapeFromLabIslandGame extends ApplicationAdapter
+public class EscapeFromLabIslandGame extends Game
 {
     SpriteBatch batch;
     Texture img;
---
+++ core/src/blackcat/labisland/systems/MovementSystem.java
@@ -0,0 +1,39 @@
+package blackcat.labisland.systems;
+
+import blackcat.labisland.components.Position;
+import blackcat.labisland.components.Velocity;
+
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.Filter;
+import com.artemis.systems.EntityProcessingSystem;
+
+public class MovementSystem extends EntityProcessingSystem
+{
+    ComponentMapper<Position> pos_mapper = null;
+    ComponentMapper<Velocity> vel_mapper = null;
+    
+    
+    @SuppressWarnings("unchecked")
+    public MovementSystem()
+    {
+        super(Filter.allComponents(Position.class, Velocity.class));
+    }
+
+    @Override
+    public void initialize()
+    {
+        pos_mapper = world.getMapper(Position.class);
+        vel_mapper = world.getMapper(Velocity.class);
+    }
+
+    @Override
+    protected void process(Entity entity)
+    {
+        Position position = pos_mapper.get(entity);
+        Velocity velocity = vel_mapper.get(entity);
+        
+        position.getPosition().x += velocity.getDX() * world.getDelta();
+        position.getPosition().y += velocity.getDY() * world.getDelta();
+    }
+}
